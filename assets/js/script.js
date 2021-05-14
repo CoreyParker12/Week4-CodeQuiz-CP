b58b9dca087992c223148029e4eb95a6b51d077d
Corey Parker <coreyparker012@gmail.com>
2021-05-13 20:03:38 -0400

    Questions work, need score and timer integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const answerBOutput = document.getElementById("answerBOutput");
 const answerCOutput = document.getElementById("answerCOutput");
 const answerDOutput = document.getElementById("answerDOutput");
 const correctIncorrect = document.getElementById('correctIncorrect');
-const nextButton = document.getElementById('nextButton');
+const scoreScreen = document.getElementById('scoreScreen').style.display = 'none';
 
 startButton.addEventListener('click', game);
 startButton.addEventListener('click', timer);
@@ -24,8 +24,8 @@ let quizQuestions = [
     },
     {
        question: "Here is a question",
-       answers: ["strings","booleance","alerts", "numbers"],
-       rightAnswer: "alerts"     
+       answers: ["1","1","1", "1"],
+       rightAnswer: "1"     
     },
     {
        question: "Question 3",
@@ -49,11 +49,15 @@ let quizQuestions = [
 
  function chosenAnswer(val) {
     let chosenAnswer = val.textContent;
-    if (quizQuestions[0].rightAnswer === chosenAnswer) {
+    if (quizQuestions[questionIndex].rightAnswer === chosenAnswer) {
             console.log('Correct');
-            questionIndex++;
-        } else console.log('Incorrect');
-
+            questionIndex++
+            game();
+        } else { 
+            console.log('Incorrect')
+            questionIndex++
+            game();
+        }
  }
 
 // Initialize Question
@@ -67,16 +71,25 @@ function game() {
     answerBOutput.innerHTML = quizQuestions[questionIndex].answers[1];
     answerCOutput.innerHTML = quizQuestions[questionIndex].answers[2];
     answerDOutput.innerHTML = quizQuestions[questionIndex].answers[3];
+
+    //Note: Using getElementById here becuase getElementsByClassName won't allow for styling
+    document.getElementById("answerAOutput").style.display = 'block';
+    document.getElementById("answerBOutput").style.display = 'block';
+    document.getElementById("answerCOutput").style.display = 'block';
+    document.getElementById("answerDOutput").style.display = 'block';
+
+    if (quizQuestions.length - 1 === questionIndex) {
+        document.getElementById("questionOutput").style.display = 'none';
+        document.getElementById("answerAOutput").style.display = 'none';
+        document.getElementById("answerBOutput").style.display = 'none';
+        document.getElementById("answerCOutput").style.display = 'none';
+        document.getElementById("answerDOutput").style.display = 'none';
+        finalScreen();
+    }
 }
 
-
-
-function nextQuestion() {
-
-questionIndex++
-console.log(questionIndex)
-game()
-
+function finalScreen () {
+    console.log('You win');
 }
 
 //Timer
